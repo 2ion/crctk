879c2eeddf73377447c1b26ed824353bc2db896c
Jens John <jens.o.john@gmail.com>
2014-01-12 17:11:50 +0900

    added GPL3 license text and -header

--- crctk.c
+++ crctk.c
@@ -1,6 +1,19 @@
 /*
  * crctk - CRC32 Hexstring Toolkit
- * Written by Jens Oliver John <asterisk!2ion!de>
+ * Copyright (C) 2014 2ion (asterisk!2ion!de)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  * The CRC32 computer is based on code by elektron, formerly available
  * at            
