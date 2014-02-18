82cfc914b5b6363944bfef6eaa294b5db634ae27
Jens John <jens.o.john@gmail.com>
2014-02-18 17:59:01 +0900

    add license headers to librealpath

--- src/realpathtest.c
+++ src/realpathtest.c
@@ -1,3 +1,22 @@
+ /*
+ * realpathtest - test routines for librealpath
+ * Copyright (C) 2014 2ion (asterisk!2ion!de)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
 #include <error.h>
 #include <errno.h>
 #include <stdlib.h>
@@ -11,7 +30,7 @@
   dir=NULL; \
   baselen=0; \
   dirlen=0; \
-  printf("*** test: my_splitpath: %s ***\n", path); \
+  printf("*******\nTEST_SPLITPATH: %s\n", path); \
   if(my_splitpath(path, &dir, &dirlen, &base, &baselen)!=0)\
     LERROR(EXIT_FAILURE,0,"my_splitpath() failed!");\
   else{\
