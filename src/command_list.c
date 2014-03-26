be8259bc4c7e24ab91bd52bccda156382f2df741
Jens John <jens.o.john@gmail.com>
2014-03-27 00:05:52 +0900

    command_list.c: use printf/fprintf instead of LERROR()

--- src/command_list.c
+++ src/command_list.c
@@ -10,10 +10,14 @@ int command_list_db(int argc, char **argv, int optind, int flags) {
   for(i = optind; i < argc; ++i) {
     DBITEM_SET_NULL(dbi);
     e = NULL;
-    if(DB_read(argv[i], &dbi) != 0)
-      LERROR(EXIT_FAILURE, 0, "Could not read database: %s", argv[i]);
-    if(dbi.kbuf == NULL)
-      LERROR(EXIT_SUCCESS, 0, "Database is empty.");
+    if(DB_read(argv[i], &dbi) != 0) {
+      fprintf(stderr, 0, "[%s] error when reading the file\n", argv[i]);
+      continue;
+    }
+    if(dbi.kbuf == NULL) {
+      printf("[%s] database is empty\n", argv[i]);
+      continue;
+    }
     e = &dbi;
     do {
       printf("[%s] %s -> %08X\n", argv[i], e->kbuf, e->crc);
