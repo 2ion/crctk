95b16ecbca30fa4a2a7eb2c93662733120509124
Jens John <dev@2ion.de>
2014-04-22 17:55:18 +0900

    scrap cwidget 'cause it's insane; use the C++ curses wrapper from ncmpcpp

--- src/ncrctk.cc
+++ src/ncrctk.cc
@@ -3,27 +3,52 @@
 
 #include <locale.h>
 #include <stdlib.h>
-#include <cwidget/toplevel.h>
-#include <cwidget/dialogs.h>
 #include <clocale>
+#include <string>
 
-extern "C" {
+extern "C"
+{
 #include "util.h"
 #include "database.h"
 }
+#include "localerror.h"
+#include "window.h"
 
-namespace cw = cwidget;
+#include "config.h"
+
+/* globals */
 
 const char *crcregex = "[[:xdigit:]]\\{8\\}";
 const char *crcregex_stripper = "[[:punct:]]\\?[[:xdigit:]]\\{8\\}[[:punct:]]\\?";
 const char *dbiofile = "crcsums.tdb";
 const char *hexarg = "00000000";
 
-int main(int argc, char **argv) {
-  cw::toplevel::init();
-  cw::widgets::widget_ref dialog = cw::dialogs::ok(L"おはよう世界！", cw::util::arg(sigc::ptr_fun(cw::toplevel::exitmain)));
-  cw::toplevel::settoplevel(dialog);
-  cw::toplevel::mainloop();
-  cw::toplevel::shutdown();
+namespace nc = NCurses;
+
+namespace ui
+{
+  namespace win
+  {
+    nc::Window *main;
+    nc::Window *log;
+  }
+}
+
+int main(int argc, char **argv)
+{
+  nc::InitScreen(PACKAGE, true);
+  ui::win::main = new nc::Window(0, 0, COLS, LINES, "", nc::clWhite, nc::brWhite);
+
+  *ui::win::main << nc::clWhite;
+  *ui::win::main << "Hello World!";
+
+  ui::win::main->Display();
+  ui::win::main->Refresh();
+
+  sleep(5);
+
+  delete ui::win::main;
+
+  nc::DestroyScreen();
   return 0;
 }
