fb84039e212e659651684fb436c6fe763409e99e
Jens John <dev@2ion.de>
2014-04-22 23:45:19 +0900

    ncrctk: the about dialog is ready ^^

--- src/ncrctk.cc
+++ src/ncrctk.cc
@@ -16,6 +16,11 @@ extern "C"
 
 #include "config.h"
 
+#define HALFDIM(x) ((x)-((x)%2))/2
+#define FORTHDIM(x) HALFDIM((HALFDIM((x))))
+#define WCENTERFS(win, str, y) (win)->GotoXY(((win)->GetWidth()-(win)->Length((str)))/2, y);
+#define WCENTERFSP(win, str, y) WCENTERFS((win),(str),(y)); *(win)<<(str);
+
 /* globals */
 
 const char *crcregex = "[[:xdigit:]]\\{8\\}";
@@ -30,25 +35,59 @@ namespace ui
   namespace win
   {
     nc::Window *main;
+    nc::Window *about;
     nc::Window *log;
+  }
+
+  void w_create_main(void)
+  {
+    win::main = new nc::Window(0, 0, COLS, LINES, "", nc::clWhite, nc::brWhite);
+    win::main->SetTitle(PACKAGE);
+  }
+
+  void w_create_about(void)
+  {
+    win::about = new nc::Window(FORTHDIM(COLS), FORTHDIM(LINES), HALFDIM(COLS), 10, "", nc::clWhite, nc::brWhite);
+    win::about->SetTitle("About");
+
+    WCENTERFS(win::about, "ncrctk "VERSION, 0);
+    *win::about << nc::fmtBold << nc::Colors(nc::clGreen, nc::clDefault) << "ncrctk " << nc::fmtBoldEnd
+      << nc::Colors(nc::clDefault, nc::clDefault) << VERSION;
+
+    WCENTERFSP(win::about, "a ncurses interface to the crcTk", 1);
+    WCENTERFSP(win::about, "Copyright (C) 2014 Jens Oliver John <"PACKAGE_BUGREPORT">", 3);
+    WCENTERFSP(win::about, "Licensed under the GNU General Public License v3 or later", 4);
+    WCENTERFSP(win::about, "Project homepage: https://github.com/2ion/crctk", 5);
+  }
+
+  void do_about(void)
+  {
+    win::about->Display();
+    win::about->ReadKey();
+    win::about->Hide();
+  }
+
+  void init(void)
+  {
+    nc::InitScreen(PACKAGE, true);
+    w_create_main();
+    w_create_about();
+    win::main->Display();
+  }
+
+  void deinit(void)
+  {
+    delete win::about;
+    delete win::main;
+    nc::DestroyScreen();
   }
 }
 
 int main(int argc, char **argv)
 {
-  nc::InitScreen(PACKAGE, true);
-  ui::win::main = new nc::Window(0, 0, COLS, LINES, "", nc::clWhite, nc::brWhite);
-
-  *ui::win::main << nc::clWhite;
-  *ui::win::main << "Hello World!";
-
-  ui::win::main->Display();
-  ui::win::main->Refresh();
-
-  sleep(5);
-
-  delete ui::win::main;
-
-  nc::DestroyScreen();
+  ui::init();
+  ui::do_about();
+  ui::win::about->ReadKey();
+  ui::deinit();
   return 0;
 }
