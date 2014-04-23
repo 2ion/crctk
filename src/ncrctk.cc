b352cebfcbc1f053eb6c3808ca48740624985d42
Jens John <dev@2ion.de>
2014-04-23 17:39:07 +0900

    ncrctk: working on the database inspector

--- src/ncrctk.cc
+++ src/ncrctk.cc
@@ -20,6 +20,10 @@ extern "C"
 #define FORTHDIM(x) HALFDIM((HALFDIM((x))))
 #define WCENTERFS(win, str, y) (win)->GotoXY(((win)->GetWidth()-(win)->Length((str)))/2, y);
 #define WCENTERFSP(win, str, y) WCENTERFS((win),(str),(y)); *(win)<<(str);
+#define SUBWINCOLS (COLS-2)
+#define SUBWINLINES (LINES-4)
+#define SUBWINOCOL 1
+#define SUBWINOLINE 3
 
 /* globals */
 
@@ -46,8 +50,12 @@ namespace ui
 
   void w_create_main(void);
   void w_create_about(void);
+  void w_create_inspector(void);
+  void w_create_error(void);
   void w_switchto(nc::Window*,bool);
+  std::string do_prompt(nc::Window*, const int * coords);
   void do_about(void);
+  void do_error(std::string &msg);
   void init(void);
   void deinit(void);
   void sighandler_resize(int);
@@ -60,12 +68,19 @@ namespace ui
 
     nc::Window *main;
     nc::Window *about;
+    nc::Window *inspector;
+    nc::Window *error;
     nc::Window *log;
-  }
 
-  namespace menu
-  {
-  
+    namespace res
+    {
+      namespace inspector
+      {
+        int dbprompt[2];
+        std::string dbpath;
+        struct DBItem dbi = DBITEM_NULL;
+      }
+    }
   }
  
   void w_create_main(void)
@@ -89,6 +104,23 @@ namespace ui
     WCENTERFSP(win::about, "Project homepage: https://github.com/2ion/crctk", 5);
   }
 
+  void w_create_error(void)
+  {
+    win::error = new nc::Window(FORTHDIM(COLS), FORTHDIM(LINES),
+        HALFDIM(COLS), 4, "", nc::clWhite, nc::brRed);
+    *win::error << "No error yet";
+  }
+
+  void w_create_inspector(void)
+  {
+    win::inspector = new nc::Window(SUBWINOCOL, SUBWINOLINE, SUBWINCOLS, SUBWINLINES, "", nc::clWhite, nc::brNone);
+    win::inspector->SetTitle("Inspector");
+
+    *win::inspector << nc::fmtBold << "Database: " << nc::fmtBoldEnd;
+    win::res::inspector::dbprompt[0] = 0 + win::inspector->Length("Database: ");
+    win::res::inspector::dbprompt[1] = 0;
+  }
+
   void do_about(void)
   {
     win::about->Display();
@@ -107,8 +139,11 @@ namespace ui
   {
     nc::InitScreen(PACKAGE, true);
     signal(SIGWINCH, sighandler_resize);
-    w_create_main();
+
     w_create_about();
+    w_create_inspector();
+    w_create_main();
+
     win::main->Display();
     win::active = win::main;
   }
@@ -138,13 +173,27 @@ namespace ui
     win::active = w;
     win::active->Display();
   }
+
+  std::string do_prompt(nc::Window *w, const int * coords)
+  {
+    w->GotoXY(coords[0], coords[1]);
+    return w->GetString(-1, w->GetWidth()-coords[0]);
+  }
+
+  void do_error(const std::string &msg)
+  {
+    win::error->Clear();
+    *win::error << msg;
+    win::error->Display();
+    win::error->ReadKey();
+    win::error->Hide();
+  }
 }
 
 
 int main(int argc, char **argv)
 {
   int input = 0;
-
 
   ui::init();
 
@@ -155,6 +204,27 @@ int main(int argc, char **argv)
     {
       ui::w_switchto(ui::win::main);
     } // win::about
+    else if(ui::win::active == ui::win::inspector)
+    {
+      if(input == 'g')
+      {
+        std::string i = ui::do_prompt(ui::win::active, ui::win::res::inspector::dbprompt);
+        if(!i.empty() &&
+            check_access_flags_v(i.c_str(), F_OK | R_OK | W_OK, 1) == 0)
+        {
+          if(DB_read(i.c_str(), &ui::win::res::inspector::dbi) != 0)
+          {
+            ui::do_error("The specified file is invalid or inaccessible.");
+          } else
+          {
+            ui::do_error("It worked!");
+          }
+        }
+      } else if(input == 'q')
+      {
+        ui::w_switchto(ui::win::main);
+      }
+    }
     else if(ui::win::active == ui::win::main)
     {
       if(input == 'q')
@@ -162,6 +232,9 @@ int main(int argc, char **argv)
       if(input == '?')
       {
         ui::w_switchto(ui::win::about);
+      } else if(input == 'i')
+      {
+        ui::w_switchto(ui::win::inspector);
       }
     } // win::main
   }
