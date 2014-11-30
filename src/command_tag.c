d89300021a53fca6abcc61a6699d435a458c1829
Jens John <dev@2ion.de>
2014-11-30 14:37:15 +0100

    cmd_tag: use continue_with_next label instead of 'continue'

--- src/command_tag.c
+++ src/command_tag.c
@@ -62,7 +62,7 @@ int command_tag(int argc, char **argv, int optind, int flags) {
       } else {
         log_failure(filename,
             "Tag already present, use --strip-tag to replace it");
-        continue;
+        goto continue_with_next;
       }
     }
     if(workstring == NULL)
