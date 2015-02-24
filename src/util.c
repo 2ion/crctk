fac598a1737bdb654724aeb10a07833d03ac1620
Jens John <dev@2ion.de>
2015-02-24 21:33:00 +0100

    util.c: simplify check_access_flags_v()

--- src/util.c
+++ src/util.c
@@ -38,20 +38,21 @@ void check_access_flags(const char *path, int access_flags, int notdir) {
   }
 }
 
-int check_access_flags_v(const char *path, int access_flags,
-    int notadir) {
+int check_access_flags_v(const char *path, int access_flags, int notadir) {
   struct stat stbuf;
   
   if(access(path, access_flags) != 0)
     return -1;
 
   if(notadir == 1) {
-    if(stat(path, &stbuf) != 0) {
-      LERROR(EXIT_FAILURE, errno,  "(unclean exit)");
-    } else
-      if(S_ISDIR(stbuf.st_mode))
-        return -1;
+    /* bad error */
+    if(stat(path, &stbuf) != 0)
+      LERROR(EXIT_FAILURE, errno,  "%s", path);
+
+    if(S_ISDIR(stbuf.st_mode))
+      return -1;
   }
+
   return 0;
 }
 
